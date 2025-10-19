4acddd95e04185ccf1f6b5af1d8fbb5a0b2ecbcf
Yadier Stiven Mayoral <yadiermayoral13@gmail.com>
2025-10-19 01:54:44 -0500

    Actualización de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+        colors: {
+            'concejo-verde-principal': 'rgb(6, 138, 79)',
+            'concejo-verde-claro': 'rgb(210, 226, 139)',
+            'concejo-azul-principal': 'rgb(0, 175, 239)',
+            'concejo-amarillo': 'rgb(255, 242, 18)',
+            'concejo-negro': 'rgb(55, 52, 53)',
+            'concejo-verde-suave': 'rgb(220, 240, 210)', // Nuevo color más suave
+        },
+        fontFamily: {
+            sans: ['"Nunito Sans"', 'sans-serif'],
+        },
+        },
+    },
+    plugins: [
+        require('@midudev/tailwind-animations')
+    ],
+};
